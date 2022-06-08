6ab33d266d9aea7909635162dc07e91cda7a6087
DZSESAKQ <jahongirabdikadirov@gmail.com>
2022-06-08 21:30:21 +0300

    1

---
+++ 1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+if __name__ == '__main__':
+ 
+    l = [random.randint(0, 99) for i in range(20)]
+    n_l=l[:]
+ 
+print(l.index(sorted(set(l))[-2]))
+ 
+y = l.index(max(l))
+l[0], l[y] = l[y], l[0]
+ 
+z = l.index(min(l))
+l[-1], l[z] = l[z], l[-1]
+ 
+print(n_l)
+print(l)
